a30cd4840240bc921904a60c0d162a3ffddd97b2
A01351095 <ego.garciaolmos@gmail.com>
2021-03-17 03:02:39 +0000

    Practica 3

---
+++ IPC/signal.c
@@ -0,0 +1,25 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int varGlobal;
+
+void signalHandler(int sig){
+	printf("Recibi segnal %d\n",sig);
+	if (sig == 2){
+		varGlobal = 1;
+	}
+}
+
+int main() {
+	signal(2, signalHandler);
+	while(1){
+		printf("Trabajando\n");
+		sleep(1);
+		if (varGlobal==1){
+			break;
+		}
+	}
+	printf("Ya voy a terminar \n");
+	return 0;
+}
