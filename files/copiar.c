bf3e29a3666d32f138ef69808c333c5173c01999
EDUARDOGO <ego.garciaolmos@gmail.com>
2021-05-25 21:52:24 +0000

    Practica 11

--- files/copiar.c
+++ files/copiar.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argnum, char **argv){
     char *original = argv[1];
     char *destino = argv[2];
+
     char buffer;
-    int size;
+    int size = 0;
+
     FILE *foriginal, *fdestino;
     foriginal = fopen(original,"rb");
     fdestino = fopen(destino,"wb");
-    while(fread(&buffer,sizeof(char),1,foriginal) ){
+
+    while(fread(&buffer,sizeof(char), 1, foriginal)){
         if(buffer == '\n'){
             char *linea;
             size += 1;
+
             fseek(foriginal, -size, SEEK_CUR);
-            linea = (char *) calloc(size, sizeof(char)); // linea[size];
-            fread(linea,sizeof(char),size,forinal)
-            if(strtr(linea,"hola") == NULL){
-                fwrite()
+            linea = (char *) calloc(size, sizeof(char)); 
+            fread(linea,sizeof(char),size,foriginal);
+
+            if(strstr(linea,"hola") == NULL){
+                fwrite(linea, size, 1, fdestino);
             }
             size = 0;
-        } else {
+        } 
+        
+        else {
             size += 1;
         }
-        fwrite(&buffer,sizeof(char),1,fdestino);
     }
+    
     fclose(foriginal);
     fclose(fdestino);
 
     return 0;
 }
-
--- files/mover.c
+++ files/mover.c
@@ -1,3 +1,4 @@
+
 // Equipo:
 // Luis Alfredo Aceves Astengo A01229441
 // Eduardo Garcia Olmos A01351095
@@ -20,7 +21,7 @@ int main(int argvnum, char **argv){
 		printf("El primer argumento es un directorio. Se espera un archivo.\n");
 		return -1;
 	}
-
+	
 	// argumento 1 si es un archivo
 	if(S_ISREG(sb.st_mode)){
 		// segundo argumento no existe, vamos a crearlo
@@ -31,7 +32,7 @@ int main(int argvnum, char **argv){
 		// segundo argumento es un directorio, vamos a mover arch1 ahi
 		else if(S_ISDIR(sb.st_mode)){
 			// se asume que path2 termina en "/"
-			link(path, path2);		// movemos el archivo al nuevo path
+			link(path, strcat(path2,path));		// movemos el archivo al nuevo path
 			unlink(path);			// borramos el viejo
 		}
 		// segundo argumento es un archivo, vamos a mandar un mensaje de error
@@ -39,6 +40,6 @@ int main(int argvnum, char **argv){
 			printf("Error: %s ya existe.\n", path2);
 		}
 	}
-
+		
 	return 1;
-} 
+}
